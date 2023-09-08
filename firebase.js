1e4ab56cab80e51d4150f89fc42ad573b41923a8
Mia7351 <139099756+Mia7351@users.noreply.github.com>
2023-09-08 21:52:59 +0900

    test success

--- firebase.js
+++ firebase.js
@@ -1,18 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  databaseURL:
-    "https://carrot-market-87fd7-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  StorageBucket: "gs://carrot-market-87fd7.appspot.com",
+  apiKey: import.meta.env.VITE_APP_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DB_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_ID,
+  appId: import.meta.env.VITE_VITE_APP_ID,
+  measurementId: "G-KFF8XZ6HXQ",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Realtime Database and get a reference to the service
+// Initialize Cloud Storage and get a reference to the service
 const database = getDatabase(app);
 const storage = getStorage(app);
+const auth = getAuth(app);
